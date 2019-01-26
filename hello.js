dcdf163022d2496468d3ea8b87e37e243dccf69d
gekongfei <389443626@qq.com>
2019-01-26 13:15:44 +0800

    first commit

---
+++ hello.js
@@ -0,0 +1,15 @@
+var { graphql, buildSchema } = require('graphql');
+var schema = buildSchema(`
+	type Query {
+		hello: String
+	}
+`);
+
+var root = {
+	hello: () => 'Hello World!'
+};
+
+graphql(schema, '{ hello }', root)
+	.then((response) => {
+		console.log(response);
+	});
